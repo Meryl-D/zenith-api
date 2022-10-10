7afa9f62a492b863f6502940ec09b414fe2e4ad1
clagnar <clarisse.gagnebin@heig-vd.ch>
2022-10-10 15:39:21 +0200

    Edited schema model for Place

--- database/model/placeModel.js
+++ database/model/placeModel.js
@@ -3,15 +3,27 @@ const Schema = mongoose.Schema;
 
 //Define a schema for Places
 const placesSchema = new Schema({
-    url_picture: String,
-    location: String,
-    description: [ // Nested array of documents
-      {
-        body: String,
-        date: Date
-      }
-    ],
-    publication_date: { type: Date, default: Date.now  }, // Default value
+    url_picture:{
+        type: String,
+        required: true,
+    }, 
+    location: {
+        type: String,
+        required: true,
+    },
+    description: // Nested array of documents
+    {
+        type: String,
+        date: Date,
+        minLenght: 2,
+        maxLength: 200,
+        required: true,
+
+    },
+    publication_date: Date, 
     visit_date: Date,
-    modification_date: Date,
+    modification_date: {
+        type: Date,
+        default: Date.now
+      }
   });
