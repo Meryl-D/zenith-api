3e5e2be5c9ddfd300dab45e5c0d5adcf63932c03
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-11-10 10:52:56 +0100

    add authorization function in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 import express from 'express';
 
+function checkAuthorization(req, res, next) {
+    if (req.userId != req.body.userId) return res.status(403).send("Unauthorized")
+    next()
+}
+
 const authRouter = express.Router();
 /* GET Authentification */
 authRouter.get('/login', function(req, res, next) {
     res.send('Login');
-});
+})
 export default authRouter;
--- routes/shared.js
+++
@@ -1,4 +0,0 @@
-function checkAuthorization(req, res, next) {
-    if (req.userId != req.body.userId) return res.status(403).send("Unauthorized")
-    next()
-}
