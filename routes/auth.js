37e22079abdcc155e5e9b17b378da1372a67e7d5
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-12-19 13:26:31 +0100

    add user to send on login

--- routes/auth.js
+++ routes/auth.js
@@ -37,8 +37,7 @@ authRouter.post("/", function (req, res, next) {
             const payload = { sub: user._id.toString(), exp: exp };
             jwt.sign(payload, secretKey, function (err, token) {
                 if (err) { return next(err); }
-                res.send({ token: token }); // Send the token to the client.
-                console.log(token)
+                res.send({ token: token, user: user }); // Send the token to the client.
             });
         });
     })
